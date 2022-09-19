63ad8cdafdace79b31beef7776274f9f15ba2e0a
missreyyo <reyhanahsen7@gmail.com>
2022-09-19 16:36:02 +0300

    fire and gun

--- Assets/Scripts/ZombieMovement.cs
+++ Assets/Scripts/ZombieMovement.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class ZombieMovement : MonoBehaviour
 {
+    private GameObject gamer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gamer = GameObject.Find("FPSController");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GetComponent<NavMeshAgent>().destination=gamer.transform.position;
     }
 }
